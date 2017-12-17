bb55a79768c364e49c24f95beac05ec6d760a320
hebaloreto92 <31828352+hebaloreto92@users.noreply.github.com>
2017-12-16 19:48:26 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
-define('DATABASE', 'hh292');
-define('USERNAME', 'hh292');
-define('PASSWORD', 'ic2BQ414k');
-define('CONNECTION', 'sql1.njit.edu');
+define('DATABASE', 'hs563');
+define('USERNAME', 'hs563');
+define('PASSWORD', 'Se1MxHdZr');
+define('CONNECTION', 'sql2.njit.edu');
 ?>
